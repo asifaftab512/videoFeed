35210b9d43e14cc0fa897330eab76d8379403853
asifaftab512 <aftab.muhammadasif@gmail.com>
2025-08-27 20:29:05 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: 'build',
+  output: 'standalone',
+};
+
+export default nextConfig
+
 // import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
